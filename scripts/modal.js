bf77cc719896161cab7df40363701f3f04af5d88
Valdecir Teixeira <vardeba@hotmail.com>
2022-09-22 12:32:15 -0300

    modal fixed and some css fixes

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,7 +1,8 @@
 const buttonsControllersModal = document.querySelectorAll("[data-control-modal]");
 
 for(let index = 0; index < buttonsControllersModal.length; index++){
-    buttonsControllersModal[index].addEventListener("click", ()=>{
+    buttonsControllersModal[index].addEventListener("click", (event)=>{
+        event.preventDefault();
         let modalId = buttonsControllersModal[index].getAttribute("data-control-modal");
         document.getElementById(modalId).classList.toggle("show-modal");
     });
