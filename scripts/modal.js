80463acfc5b03f403df86143b6cc160b384cfb15
Valdecir Teixeira <vardeba@hotmail.com>
2022-09-22 03:24:35 -0300

    modal error

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,5 +4,13 @@ for(let index = 0; index < buttonsControllersModal.length; index++){
     buttonsControllersModal[index].addEventListener("click", ()=>{
         let modalId = buttonsControllersModal[index].getAttribute("data-control-modal");
         document.getElementById(modalId).classList.toggle("show-modal");
-    })
+    });
 }
+
+
+
+
+
+
+
+
