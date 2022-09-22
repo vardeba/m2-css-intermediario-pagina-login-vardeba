fc469aff40234fdf02d4e2edcee5b16865246eba
Valdecir Teixeira <vardeba@hotmail.com>
2022-09-22 01:35:22 -0300

    first commit: modal implemented

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,8 @@
-/* Desenvolva seu código aqui... */
+const buttonsControllersModal = document.querySelectorAll("[data-control-modal]");
+
+for(let index = 0; index < buttonsControllersModal.length; index++){
+    buttonsControllersModal[index].addEventListener("click", ()=>{
+        let modalId = buttonsControllersModal[index].getAttribute("data-control-modal");
+        document.getElementById(modalId).classList.toggle("show-modal");
+    })
+}
